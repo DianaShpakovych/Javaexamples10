0dacbb429fb904266b1e760403f0aa8c85f8d41f
DianaShpakovych <dianashpakovych@gmail.com>
2020-10-04 16:55:43 +0200

    Initial commit

---
+++ src/JavaProgram.java
@@ -0,0 +1,11 @@
+public class JavaProgram {
+    public static void main(String args[])
+    {
+        int n=1, i;
+        for(i=0; i<20; i++)
+        {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
